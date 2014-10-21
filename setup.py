99ea9c58955d58d5ff37c566957ab3a6c7b4e891
Mohab Usama <mohab.usama@gmail.com>
2014-10-22 00:02:14 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_require=['nose', 'mock'],
     test_suite='nose.collector',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
